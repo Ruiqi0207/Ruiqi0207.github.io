e7f66e63ea74426c786dc077ad98e97ed539fdc9
Ruiqi0207 <15860160743@163.com>
2026-06-13 12:01:30 +0800

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-(function() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const lang = urlParams.get('lang');
-    if (lang === 'en') {
-        document.body.classList.add('lang-en');
-    }
-})();
